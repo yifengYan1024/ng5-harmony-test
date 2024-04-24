b2e0f1743eca9c35a8ae07e2596a0be0af90c8eb
yanyifeng <13432827297@163.com>
2024-04-24 22:01:06 +0800

    init

---
+++ src/app/app.module.ts
@@ -0,0 +1,42 @@
+﻿import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule} from '@angular/platform-browser/animations'
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { MatButtonModule, MatCardModule, MatInputModule, MatSnackBarModule, MatToolbarModule } from '@angular/material';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms'
+import { EqualValidatorDirective } from './shared/equal.validator.directive';
+
+import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { NavComponent } from './nav/nav.component';
+import { RegistrationComponent } from './registration/registration.component';
+import { LoginComponent } from './login/login.component';
+
+import { AuthService } from './auth.service';
+import { AuthGuard } from './auth.guard';
+
+const myRoots: Routes = [
+  { path: '', component: HomeComponent, pathMatch: 'full' , canActivate: [AuthGuard]},
+  { path: 'register', component: RegistrationComponent },
+  { path: 'login', component: LoginComponent},
+  { path: 'home', component: HomeComponent, canActivate: [AuthGuard]}
+];
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    NavComponent,
+    RegistrationComponent,
+    LoginComponent,
+    EqualValidatorDirective
+  ],
+  imports: [
+    BrowserModule, BrowserAnimationsModule, FormsModule, ReactiveFormsModule,
+    MatButtonModule, MatCardModule, MatInputModule, MatSnackBarModule, MatToolbarModule,
+    RouterModule.forRoot(myRoots)
+  ],
+  providers: [AuthService, AuthGuard],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/registration/registration.component.ts
@@ -0,0 +1,22 @@
+﻿import { OnInit, Component } from "@angular/core";
+import { User } from '../models/user.model';
+
+@Component({
+  selector: 'app-registration',
+  templateUrl: './registration.component.html',
+  styleUrls: ['./registration.component.css']
+})
+export class RegistrationComponent implements OnInit {
+  user = new User();
+  constructor() {
+  }
+
+  ngOnInit() {
+  }
+
+  register(user: User): void{
+    console.log(user);
+  }
+}
+
+
